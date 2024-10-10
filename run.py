31e6ddb14ae8a496c04a5cd30719dbfad68160e6
Smithk0 <smithkent102@gmail.com>
2024-10-10 18:53:44 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 # run.py
+
 from app.routes import app
+import hypercorn.asyncio
+from hypercorn.config import Config
+import asyncio
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    config = Config()
+    config.bind = ["0.0.0.0:8000"]  # Listen on all interfaces
+    config.workers = 2  # Number of worker processes
+    config.loglevel = "info"  # Logging level
+    config.use_reloader = False  # Disable auto-reload in production
+    asyncio.run(hypercorn.asyncio.serve(app, config))
